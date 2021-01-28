a4d826545f4fa4a6ccb541edf55a6c62b91c67c6
AndrewMorrow <beardedmongo@gmail.com>
2021-01-28 00:22:44 -0600

    adds system logs and logsitem

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from "react";
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 import "./App.css";
+import Searchbar from "./components/layout/Searchbar";
+import Logs from "./components/logs/Logs";
+import AddBtn from "./components/layout/AddBtn";
+import AddLogModal from "./components/logs/AddLogModal";
 
 const App = () => {
     useEffect(() => {
@@ -9,9 +13,14 @@ const App = () => {
         M.AutoInit();
     });
     return (
-        <div className="App">
-            <header className="App-header">App</header>
-        </div>
+        <>
+            <Searchbar />
+            <div className="container">
+                <AddBtn />
+                <AddLogModal />
+                <Logs />
+            </div>
+        </>
     );
 };
 
