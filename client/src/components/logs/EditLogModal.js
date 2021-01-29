01bfaff646b056986d056b4c18ffd9306da89bf9
AndrewMorrow <beardedmongo@gmail.com>
2021-01-29 13:27:10 -0600

    Update Logs implemented

--- client/src/components/logs/EditLogModal.js
+++ client/src/components/logs/EditLogModal.js
@@ -1,16 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { updateLog } from "../../actions/logActions";
 import M from "materialize-css/dist/js/materialize.min.js";
 
-const EditLogModal = () => {
+const EditLogModal = ({ current, updateLog }) => {
     const [message, setMessage] = useState("");
     const [attention, setAttention] = useState(false);
     const [tech, setTech] = useState("");
+
+    useEffect(() => {
+        if (current) {
+            setMessage(current.message);
+            setAttention(current.attention);
+            setTech(current.tech);
+        }
+    }, [current]);
 
     const onSubmit = () => {
         if (message === "" || tech === "") {
             M.toast({ html: "Please enter a message and select a tech." });
         } else {
-            console.log(message, tech, attention);
+            const updtLog = {
+                id: current.id,
+                message,
+                attention,
+                tech,
+                date: new Date(),
+            };
+            updateLog(updtLog);
+            M.toast({ html: `Log Updated by ${tech}` });
             // Clear Fields
             setMessage("");
             setTech("");
@@ -21,7 +40,7 @@ const EditLogModal = () => {
     return (
         <div id="edit-log-modal" className="modal" style={modalStyle}>
             <div className="modal-content">
-                <h4>Enter System Log</h4>
+                <h4>Update System Log</h4>
                 <div className="row">
                     <div className="input-field">
                         <input
@@ -30,9 +49,6 @@ const EditLogModal = () => {
                             value={message}
                             onChange={(e) => setMessage(e.target.value)}
                         />
-                        <label htmlFor="message" className="active">
-                            Log Message
-                        </label>
                     </div>
                 </div>
                 <div className="row">
@@ -87,4 +103,13 @@ const modalStyle = {
     height: "75%",
 };
 
-export default EditLogModal;
+EditLogModal.propTypes = {
+    current: PropTypes.object,
+    updateLog: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+    current: state.log.current,
+});
+
+export default connect(mapStateToProps, { updateLog })(EditLogModal);
--- client/src/reducers/logReducer.js
+++ client/src/reducers/logReducer.js
@@ -4,6 +4,9 @@ import {
     LOGS_ERROR,
     ADD_LOG,
     DELETE_LOG,
+    SET_CURRENT,
+    CLEAR_CURRENT,
+    UPDATE_LOG,
 } from "../actions/types";
 
 const initialState = {
@@ -37,6 +40,25 @@ export default (state = initialState, action) => {
                 ...state,
                 logs: state.logs.filter((log) => log.id !== action.payload),
                 loading: false,
+            };
+        case UPDATE_LOG:
+            return {
+                ...state,
+                logs: state.logs.map((log) =>
+                    log.id === action.payload.id ? action.payload : log
+                ),
+                loading: false,
+            };
+        case SET_CURRENT:
+            console.log(action.payload);
+            return {
+                ...state,
+                current: action.payload,
+            };
+        case CLEAR_CURRENT:
+            return {
+                ...state,
+                current: null,
             };
         case LOGS_ERROR:
             console.log(action.payload);
